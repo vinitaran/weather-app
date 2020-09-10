b731b3cbbe7331398b7bade0bc2b09e2c1d5df2d
root <root@DESKTOP-RVT825A.localdomain>
2020-09-10 18:10:00 +0530

    Updates

---
+++ precache-manifest.82310f4254f3776c9cd5a7e3a2263d12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e7e24f77aeeb94a65a8edfa28e3261a",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "255be82494f112657af0",
+    "url": "/weather-app/static/css/main.a4b7d7a6.chunk.css"
+  },
+  {
+    "revision": "e53ab5dca58e758eca48",
+    "url": "/weather-app/static/js/2.ead68388.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.ead68388.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "255be82494f112657af0",
+    "url": "/weather-app/static/js/main.3c6b98e1.chunk.js"
+  },
+  {
+    "revision": "ab444e91d1aad080060e",
+    "url": "/weather-app/static/js/runtime-main.cabe57b0.js"
+  }
+]);
